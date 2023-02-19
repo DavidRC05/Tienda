7b61b4ea8deb4a1b43eb5763a22f665524695821
DavidRC05 <105888182+DavidRC05@users.noreply.github.com>
2023-02-18 22:21:05 -0600

    Arreglado el problema del proyecto duplicado

---
+++ src/main/java/com/Tienda/dao/ClienteDao.java
@@ -0,0 +1,13 @@
+
+package com.Tienda.dao;
+
+import com.Tienda.domain.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author davidrodriguez
+ */
+public interface ClienteDao extends  CrudRepository<Cliente, Long>{
+    
+}
